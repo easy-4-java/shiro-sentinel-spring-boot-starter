7be8a4171f13341aea906f56ee9abf1776e439cd
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 12:31:19 +0800

    docs: add comprehensive English Javadoc

--- src/main/java/org/apache/shiro/spring/boot/ShiroSentinelProperties.java
+++ src/main/java/org/apache/shiro/spring/boot/ShiroSentinelProperties.java
@@ -26,6 +26,7 @@ import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 
 
 @ConfigurationProperties(ShiroSentinelProperties.PREFIX)
+/**\n * Configuration properties for Shiro Sentinel.\n *\n * @author [@Loong Wan](https://github.com/loong10k)\n * @since 1.0.0\n */
 public class ShiroSentinelProperties{
 
 	public static final String PREFIX = "shiro.sentinel";
